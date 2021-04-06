93005e5656a629ec4119b5a8fd62f116b46d8755
Rafa Zoldan <zoldan.rafael@gmail.com>
2021-04-07 01:12:08 +0300

    :sparkles: add general structure, header, temp DB

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
 import React from 'react';
+import Header from './components/Header.jsx';
+import CardList from './components/card/CardList.jsx';
 
-const App = () => <div className="App w-full h-10 bg-blue-500">HELLO</div>;
+const App = () => (
+  <div className="App bg-black mt-10 text-white">
+    <Header />
+    <div className=" max-w-screen-xl m-auto">
+      <h1 className="text-2xl">
+        Dark Mode Color Palettes for Designers and Artists
+      </h1>
+      <h2>
+        Night Palette is a free inspirational color palette tool made for dark
+        mode design
+      </h2>
+      <CardList />
+    </div>
+  </div>
+);
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => (
+  <div className="w-full h-10 bg-black text-white fixed top-0">
+    Night Palette
+  </div>
+);
+
+export default Header;
