ec97c232d5ec93eec4510f41536e3fc33b5ff50c
Rafa Zoldan <zoldan.rafael@gmail.com>
2021-04-16 17:11:30 +0300

    :sparkles: add card component

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import Palette from './Palette.jsx';
-import Star from '../../assets/icons/star.svg';
+import Palette from '../shared/Palette.jsx';
+import Star from '../../images/icons/star.svg';
 
 const Card = ({ palette, id, expanded, shrink, className, ...props }) => {
   return (
     <div
       className={[
-        'mx-4 px-4 pt-3 bg-card-gray shadow-main rounded-md mb-20',
+        'mx-4 px-4 pt-3 bg-card-gray shadow-main rounded-md mb-20 cursor-pointer',
         expanded ? 'w-full' : shrink ? 'w-14' : 'w-60',
         className,
       ]
@@ -14,7 +14,7 @@ const Card = ({ palette, id, expanded, shrink, className, ...props }) => {
         .join(' ')}
       {...props}
     >
-      <Palette colors={palette} />
+      <Palette colors={palette} className="h-56"/>
       <div className="flex justify-between h-16 items-center">
         <div className="text-base flex items-center">
           <Star className="w-3 h-3 mr-3" />
