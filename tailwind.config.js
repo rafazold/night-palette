dbd986b20d6bdd8b10f1e5d1425d523d2f60eed2
Rafa Zoldan <zoldan.rafael@gmail.com>
2021-05-04 23:20:03 +0300

    :sparkles: add fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,29 @@
 module.exports = {
-  purge: [],
+  purge: {
+    content: [
+      'src/components/**/*.jsx',
+      'src/components/**/*.js',
+      'src/index.js',
+    ],
+    whitelist: ['font-primary'],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
         'card-gray': '#191919',
+        'button-gray': '#262626',
       },
       spacing: {
         '1/10': '10%',
-        40: '31.25rem'
+        40: '31.25rem',
       },
       boxShadow: {
         main: '0 3px 20px 0 rgba(0, 0, 0, 0.45)',
       },
-    
+      fontFamily: {
+        primary: ['Montserrat', 'sans-serif'],
+      },
     },
   },
   variants: {
