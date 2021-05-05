0d9c896f95616d5af6b5ac9091b52588fb4ae84f
Rafael Zoldan <rafael@techmarketing.co.il>
2021-05-05 23:22:23 +0300

    :recycle: change data structure to arr of objs with id and colors keys

--- src/context/store.js
+++ src/context/store.js
@@ -5,15 +5,15 @@ import defaultPalettes from '../assets/default-palettes.json';
 export default () => {
   const [customPalettes, setCustomPalettes] = usePersistState(
     'customPalettes',
-    {}
+    []
   );
   const [selectedCard, setSelectedCard] = useState('');
   const addCustomPalette = ({ paletteId, colors }) => {
-    if (customPalettes[paletteId]) {
+    if (customPalettes.some((item) => item.id === paletteId)) {
       alert('id exists');
     } else {
       customPalettes[paletteId] = colors;
-      setCustomPalettes({ ...customPalettes });
+      setCustomPalettes([...customPalettes, { id: paletteId, colors }]);
     }
   };
   return {
