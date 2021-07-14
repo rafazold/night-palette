5bfc1aed95ad93b97d6f8960e77e33e849692125
Rafael Zoldan <rafael@techmarketing.co.il>
2021-07-15 00:37:23 +0300

    :recycle: add linkedin links to footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,22 @@ import Heart from '../../assets/images/icons/heart.svg';
 
 const Footer = () => (
   <div className="w-full flex justify-center items-center text-white fixed bottom-0 left-0 py-1 bg-black backdrop-filter backdrop-blur-lg bg-opacity-90">
-    Made with <Heart className="h-3 text-button-green mx-2" /> by Gil & Rafa
+    Made with <Heart className="h-3 text-button-green mx-2" /> by
+    <a
+      href="https://www.linkedin.com/in/finkelsteingil/"
+      target="_blank"
+      className="mx-1"
+    >
+      Gil
+    </a>{' '}
+    &
+    <a
+      href="https://www.linkedin.com/in/rafa-zoldan/?lipi=urn%3Ali%3Apage%3Ad_flagship3_feed%3BQgKAnVvKT0WukK6%2FPKDbMw%3D%3D"
+      target="_blank"
+      className="mx-1"
+    >
+      Rafa
+    </a>
   </div>
 );
 
