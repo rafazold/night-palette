cc81d45b688525869a32873f8e9e3cc36593efd1
Rafa Zoldan <zoldan.rafael@gmail.com>
2021-07-16 23:45:48 +0300

    :sparkles: add button component

---
+++ src/components/shared/Button.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Button = ({ children, className, secondary = false, ...props }) => (
+  <button
+    {...props}
+    className={[
+      'py-2',
+      'lg:py-1',
+      'px-2',
+      'my-auto',
+      'rounded-lg',
+      'bg-gradient-to-r',
+      secondary ? 'bg-button-gray' : 'from-button-green',
+      'to-button-blue',
+      'text-black',
+      'text-xs',
+      'focus:outline-none',
+      'lg:text-sm',
+      className,
+    ]
+      .filter(Boolean)
+      .join(' ')}
+  >
+    {children}
+  </button>
+);
+
+export default Button;
