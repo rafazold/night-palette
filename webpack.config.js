209a7ba1360ecfa1306dfee85e7759a13bea20da
Rafa Zoldan <zoldan.rafael@gmail.com>
2021-07-18 15:45:17 +0300

    :recycle: add og images with webpack copy plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const HtmlPlugin = require('html-webpack-plugin');
-
-const { resolve } = require('path');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const { resolve, join } = require('path');
 
 const publicPath = process.env.PUBLIC_PATH || '/';
 
@@ -34,6 +35,19 @@ module.exports = {
         test: /\.svg$/,
         use: ['@svgr/webpack'],
       },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'assets/images',
+            },
+          },
+        ],
+      },
+      { test: /\.html$/, loader: 'html-loader' },
     ],
   },
   resolve: {
@@ -49,9 +63,56 @@ module.exports = {
         viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no',
         description: 'Dark Mode Color Palettes for Designers and Artists',
         'theme-color': '#000000',
+        'og:title': { property: 'og:title', content: 'Night Palette' },
+        'og:description': {
+          property: 'og:description',
+          content: 'Dark Mode Color Palettes for Designers and Artists',
+        },
+        'og:type': { property: 'og:type', content: 'website' },
+        'og:url': { property: 'og:url', content: 'https://nightpalette.com' },
+        'og:image': {
+          property: 'og:image',
+          content: '/assets/images/night-share.png',
+        },
+        'twitter:card': {
+          name: 'twitter:card',
+          content: 'summary_large_image',
+        },
+        'twitter:title': { name: 'twitter:title', content: 'Night Palette' },
+        'twitter:description': {
+          name: 'twitter:description',
+          content: 'Dark Mode Color Palettes for Designers and Artists',
+        },
+        'twitter:image': {
+          name: 'twitter:image',
+          content: '/assets/images/night-share.png',
+        },
       },
       title: 'Night Palette',
       favicon: './src/assets/favicon.ico',
+    }),
+    new WebpackPwaManifest({
+      name: 'Night Palette',
+      short_name: 'Night-Palette',
+      description: 'Dark Mode Color Palettes for Designers and Artists',
+      background_color: '#000',
+      icons: [
+        {
+          src: resolve('./src/assets/images/night-logo.png'),
+          sizes: [120, 152, 167, 180, 192, 512, 1024],
+          destination: join('assets'),
+          ios: true,
+        },
+        {
+          src: resolve('./src/assets/images/night-logo.png'),
+          size: 1024,
+          destination: join('assets'),
+          ios: 'startup',
+        },
+      ],
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: 'src/assets/images', to: 'assets/images' }],
     }),
   ],
 };
