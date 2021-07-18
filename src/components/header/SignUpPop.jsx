662183cfb3df6a8f69b111b0328af41e3a24a89b
Rafael Zoldan <rafael@techmarketing.co.il>
2021-07-19 01:06:57 +0300

    :bug: fix mobile menu out of boundaries

--- src/components/header/SignUpPop.jsx
+++ src/components/header/SignUpPop.jsx
@@ -9,7 +9,7 @@ const SignUpPop = ({ user, signOut, children }) => {
       buttonIcon={<User className="w-8 h-8" data-button="true" />}
       open={openSignUp}
       handleOpen={setOpenSignUp}
-      className="top-16 right-0 w-96"
+      className="top-16 right-0 w-80"
     >
       <div
         className={[
