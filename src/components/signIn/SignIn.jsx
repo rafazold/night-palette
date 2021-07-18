4fff4def54d2e1171582e4d09b5493cc542bfdb9
Rafael Zoldan <rafael@techmarketing.co.il>
2021-07-18 16:12:29 +0300

    :recycle: add bullet points on reasons to sign in

--- src/components/signIn/SignIn.jsx
+++ src/components/signIn/SignIn.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useContext } from 'react';
 import context from '../../context/context';
 import { useHistory } from 'react-router-dom';
+import CheckIcon from '../../assets/images/icons/check-icon.svg';
 
 import { firebase, emailProvider, googleProvider } from '../../api/firebase';
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
@@ -30,8 +31,26 @@ const SignIn = () => {
   return (
     <>
       {!user ? (
-        <div>
-          <p>Please sign-in:</p>
+        <div className="container mx-auto flex flex-col justify-center">
+          <div className="lg:w-1/2 text-white mx-auto my-10">
+            <p className="text-xl mb-6">
+              Login and Start Your Night Palette Experience:
+            </p>
+            <ul>
+              {[
+                'Save your favorite palettes.',
+                'Like other users palettes',
+                'Private page for your added and liked  palettes.',
+              ].map((bullet) => (
+                <li className="flex items-center">
+                  <span className="border border-white mr-2 h-4 w-4 flex justify-center">
+                    <CheckIcon className="h-3" />
+                  </span>
+                  <span>{bullet}</span>
+                </li>
+              ))}
+            </ul>
+          </div>
           <StyledFirebaseAuth
             uiConfig={uiConfig}
             firebaseAuth={firebase.auth()}
